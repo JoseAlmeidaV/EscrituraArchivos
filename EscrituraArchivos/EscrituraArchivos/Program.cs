a94b80d7fb72b96bc578efedd247de57816ccc30
JoseAlmeidaV <105743123+JoseAlmeidaV@users.noreply.github.com>
2022-06-02 10:41:14 -0700

    v4
    tarea 02/06/22

--- EscrituraArchivos/EscrituraArchivos/Program.cs
+++ EscrituraArchivos/EscrituraArchivos/Program.cs
@@ -11,14 +11,15 @@ namespace EscrituraArchivos
     { 
         internal class Persona
         {
-            public string nombre, apellidos, estudios;
+            public string nombre, apellidos, estudios, universidad;
             public int edad;
             
-            public Persona(string nombre, string apellido, string estudios, int edad)
+            public Persona(string nombre, string apellido, string estudios, string universidad, int edad)
             {
                 this.nombre = nombre;
                 this.apellidos = apellido;
                 this.estudios = estudios;
+                this.universidad = universidad;
                 this.edad = edad;
             }
             public void Resultados()
@@ -27,6 +28,7 @@ namespace EscrituraArchivos
                 Console.WriteLine("nombre: " + nombre);
                 Console.WriteLine("Apellido: " + apellidos);
                 Console.WriteLine("Estudios: " + estudios);
+                Console.WriteLine("universidad: " + universidad);
                 Console.WriteLine("Edad: " + edad);
             }
         }
@@ -37,12 +39,14 @@ namespace EscrituraArchivos
             string nombre = Console.ReadLine();
             Console.Write("Coloque el apellido parterno: ");
             string apellido = Console.ReadLine();
-            Console.WriteLine("Coloque su grado de estudio: ");
+            Console.Write("Coloque su grado de estudio: ");
             string estudios = Console.ReadLine();
-            Console.Write("Coloque su edad: ");
+            Console.Write("Coloque su universidad: ");
+            string universidad = Console.ReadLine();
+            Console.WriteLine("coloque su edad: ");
             int edad = int.Parse(Console.ReadLine());
 
-            Persona P1 = new Persona(nombre, apellido, estudios, edad);
+            Persona P1 = new Persona(nombre, apellido, estudios, universidad, edad);
             P1.Resultados();
             sw.Close(); // siempre se debe cerrar el archivo
             Console.ReadKey();
